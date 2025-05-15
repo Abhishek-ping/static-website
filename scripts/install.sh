21546a7181855406de7ddced19abccf25a37bdb0
root <root@ip-10-0-1-233.ap-south-1.compute.internal>
2025-05-15 11:13:54 +0000

    Initial commit for new static website project

---
+++ scripts/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Running install script..."
+# e.g., apt install nginx -y or yum install httpd -y
---
+++ scripts/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Starting application..."
+# e.g., systemctl start nginx or httpd
